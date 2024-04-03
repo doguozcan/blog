80e58a221d84251936ffaf74c579f2662635f89d
doguozcan <76822280+doguozcan@users.noreply.github.com>
2024-04-03 18:18:08 +0300

    Add express's json middleware

--- server/app.js
+++ server/app.js
@@ -4,9 +4,7 @@ const app = express()
 const port = 3000
 const mongoose = require('mongoose')
 
-app.get('/', (req, res) => {
-  res.send('blog')
-})
+app.use(express.json())
 
 const start = async () => {
   try {
