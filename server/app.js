2e3a4a2ae04aff1f26deb3f172e46f9cf3d388b4
doguozcan <76822280+doguozcan@users.noreply.github.com>
2024-04-03 17:53:51 +0300

    Implement MongoDB connection

--- server/app.js
+++ server/app.js
@@ -1,9 +1,21 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const port = 3000
+const mongoose = require('mongoose')
 
 app.get('/', (req, res) => {
   res.send('blog')
 })
 
-app.listen(port, () => console.log(`Listening on port ${port}`))
+const start = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI)
+    console.log('Connected to the DB successfully!')
+    app.listen(port, () => console.log(`Listening on port ${port}`))
+  } catch (error) {
+    console.log(error.message)
+  }
+}
+
+start()
