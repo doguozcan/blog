8fe522621d4b1ec86d49359dd03578abdb28e699
doguozcan <76822280+doguozcan@users.noreply.github.com>
2024-04-05 11:49:25 +0300

    Add posts component to the app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,16 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Layout from './components/Layout'
+import Posts from './features/post/Posts'
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Layout />}></Route>
+        <Route path="/" element={<Layout />}>
+          <Route path="post">
+            <Route index element={<Posts />} />
+          </Route>
+        </Route>
       </Routes>
     </Router>
   )
