17b5c2cf9da66c02232a32cb85b5cc4f03ee162b
doguozcan <76822280+doguozcan@users.noreply.github.com>
2024-04-10 20:52:58 +0300

    Disable redux dev tools

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -7,4 +7,5 @@ export default configureStore({
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(apiSlice.middleware),
+  devTools: false,
 })
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,6 +6,10 @@ import { Provider } from 'react-redux'
 import store from './app/store.js'
 import { disableReactDevTools } from '@fvilers/disable-react-devtools'
 
+if (process.env.NODE_ENV === 'production') {
+  disableReactDevTools()
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <App />
