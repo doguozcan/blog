a76c0c7e4a41e89a558a0ab9e498d25cdc5af1a6
doguozcan <76822280+doguozcan@users.noreply.github.com>
2024-04-10 20:51:54 +0300

    Disable react devtools

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,6 +4,7 @@ import App from './App.jsx'
 import './index.css'
 import { Provider } from 'react-redux'
 import store from './app/store.js'
+import { disableReactDevTools } from '@fvilers/disable-react-devtools'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
